6d2b83fdf6deb651583250d1431718201b989874
NathanielYoung <young.nathaniel95@gmail.com>
2019-10-14 11:47:41 -0600

    Navbar return button

--- src/components/parts/navbar.js
+++ src/components/parts/navbar.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import * as actions from '../reducers/actions'
+import { Link } from 'react-router-dom'
 
 class NavBar extends Component {
 
   render() {
     return (
       <div>
+        <Link to='/'>
+          return
+        </Link>
       </div>
     );
   }
