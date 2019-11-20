50c7fbca075e7c54a1a904610f6565adfb2e4ed6
RazorzMcLazorz <young.nathaniel95@gmail.com>
2019-11-19 19:54:04 -0700

    back button added

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import * as actions from '../reducers/actions'
-import Footer from './parts/footer'
+import Navbar from './parts/navbar'
 
 class Contact extends Component {
 
@@ -12,27 +12,28 @@ class Contact extends Component {
   render() {
     return (
       <div id='contact'>
-          <div className = "ContactInfo">
-              <div>You can find me at these links</div>
-              <div className="MYLinks">
-                <a href="https://github.com/RazorzMcLazorz" target="_blank"><i class="fab fa-github-square"></i></a>
-                <a href="https://www.facebook.com/RazorzMcLazorz" target="_blank"><i class="fab fa-facebook"></i></a>
-                <a href="https://www.linkedin.com/in/nate-p-young/" target="_blank"><i class="fab fa-linkedin"></i></a>
-              </div>
-              <div>Or</div>
-              <div className="MYLinksTop">Manually message me at: </div>
+        <Navbar/>
+        <div className = "ContactInfo">
+            <div>You can find me at these links</div>
+            <div className="MYLinks">
+              <a href="https://github.com/RazorzMcLazorz" target="_blank"><i class="fab fa-github-square"></i></a>
+              <a href="https://www.facebook.com/RazorzMcLazorz" target="_blank"><i class="fab fa-facebook"></i></a>
+              <a href="https://www.linkedin.com/in/nate-p-young/" target="_blank"><i class="fab fa-linkedin"></i></a>
+            </div>
+            <div>Or</div>
+            <div className="MYLinksTop">Manually message me at: </div>
 
-              <div className="MYLinksTop3" type="text" id ="email" value="nateyoungdev@gmail.com">nateyoungdev@gmail.com</div>
+            <div className="MYLinksTop3" type="text" id ="email" value="nateyoungdev@gmail.com">nateyoungdev@gmail.com</div>
 
-              <div className="MYLinksTop">To make it faster here's some links to the Emails</div>
-              <div className="EmailLinks">
-                  <a href="https://mail.google.com/" target="_blank" className="projects"><img src = "\assets\gmail.png"></img></a>
-                  <a href="https://outlook.live.com/owa/" target="_blank" className="projects"><img src = "\assets\Outlook.png"></img></a>
-                  <a href="https://overview.mail.yahoo.com/" target="_blank" className="projects"><img src = "\assets\yahoo.png"></img></a>
-                  <a href="http://www.inbox.com/tech/" target="_blank" className="projects"><img src = "\assets\inbox.png"></img></a>
-                  <a href="https://www.icloud.com/" target="_blank" className="projects"><img src = "\assets\icloud.png"></img></a>
-              </div>
-          </div>
+            <div className="MYLinksTop">To make it faster here's some links to the Emails</div>
+            <div className="EmailLinks">
+                <a href="https://mail.google.com/" target="_blank" className="projects"><img src = "\assets\gmail.png"></img></a>
+                <a href="https://outlook.live.com/owa/" target="_blank" className="projects"><img src = "\assets\Outlook.png"></img></a>
+                <a href="https://overview.mail.yahoo.com/" target="_blank" className="projects"><img src = "\assets\yahoo.png"></img></a>
+                <a href="http://www.inbox.com/tech/" target="_blank" className="projects"><img src = "\assets\inbox.png"></img></a>
+                <a href="https://www.icloud.com/" target="_blank" className="projects"><img src = "\assets\icloud.png"></img></a>
+            </div>
+        </div>
       </div>
     );
   }
