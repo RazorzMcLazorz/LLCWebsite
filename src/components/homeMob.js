8bf7a2281dc68588dbf1583f59cf0fdab7afaebc
RazorzMcLazorz <nateyoungdev@gmail.com>
2020-07-24 01:13:35 -0600

    caught up mobile to desktop

--- src/components/homeMob.js
+++ src/components/homeMob.js
@@ -13,7 +13,32 @@ class HomeMob extends Component {
     return (
       <div id='homeMob'>
         <div id='mobile'>
-          <div id='mobileName'>Nathaniel P. Young</div>
+          <div id='mobileName'>
+            <h4 className='mobHomeName'>
+              <span>N</span>
+              <span>a</span>
+              <span>t</span>
+              <span>h</span>
+              <span>a</span>
+              <span>n</span>
+              <span>i</span>
+              <span>e</span>
+              <span>l</span>
+              <pre> </pre>
+              <span>P</span>
+              <span>.</span>
+              <pre> </pre>
+              <span>Y</span>
+              <span>o</span>
+              <span>u</span>
+              <span>n</span>
+              <span>g</span>
+            </h4>
+          </div>
+          <h6>Frontend Developer</h6>
+          <div id='mobileLogo'>
+            <img src='assets/Logo 1.png' />
+          </div>
           <div id='mobileLinks'>
             <Link to='/projects' className='mobileLinkButtons'>
               Projects
