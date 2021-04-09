8c72ef679cd105d497e4e2e09ad79d57ea32b0e5
RazorzMcLazorz <nateyoungdev@gmail.com>
2021-04-09 00:58:41 -0600

    added another ring, changed the speed, and text

--- src/components/parts/skillsSpin.js
+++ src/components/parts/skillsSpin.js
@@ -2,7 +2,18 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import * as actions from '../../reducers/actions'
 
-const LanguageWords = ['React', 'Javascript', 'Python', 'HTML', 'SCSS', 'Typescript']
+const LanguageWordsOuter = [
+  'React.js',
+  'Javascript',
+  'Python',
+  'HTML',
+  'SCSS',
+  'Typescript',
+  'Redux.js',
+  'REST',
+  'HTTP',
+]
+const LanguageWordsInner = ['AWS Amplify', 'BASH', 'ZSH', 'GIT', 'MySQL', 'Node.js']
 
 /**
  * @param small boolean
@@ -11,11 +22,19 @@ class SkillsSpin extends Component {
   render() {
     return (
       <div id='skillsSpinContainer'>
-        {LanguageWords.map((word, indx) => (
+        {LanguageWordsOuter.map((word, indx) => (
           <div
             className='spin'
-            key={'skillSpin' + indx}
-            style={{ animationDelay: '-' + indx + 's' }}>
+            key={'skillSpinOuter' + indx}
+            style={{ animationDelay: '-' + indx * 1.1 + 's' }}>
+            <div className='words'>{word}</div>
+          </div>
+        ))}
+        {LanguageWordsInner.map((word, indx) => (
+          <div
+            className='spinInner'
+            key={'skillSpinInner' + indx}
+            style={{ animationDelay: '-' + indx * 1.65 + 's' }}>
             <div className='words'>{word}</div>
           </div>
         ))}
