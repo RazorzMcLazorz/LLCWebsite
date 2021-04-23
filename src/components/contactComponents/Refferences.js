55e4475859e71d2f3d4c936681618c04b59927c7
RazorzMcLazorz <nateyoungdev@gmail.com>
2021-04-23 00:30:36 -0600

    Refferences ready

--- src/components/contactComponents/Refferences.js
+++ src/components/contactComponents/Refferences.js
@@ -3,10 +3,39 @@ import { connect } from 'react-redux'
 import * as actions from '../../reducers/actions'
 
 class Refferences extends Component {
+  state = {
+    refferences: [
+      {
+        name: 'Buck Brady',
+        affiliation: 'CTO at Rovitracker.inc',
+        phoneNumber: '385-207-6257',
+        email: 'bbrady145@gmail.com',
+        HIGTK: '',
+      },
+      {
+        name: 'Taylor Millar',
+        affiliation: 'Backend Developer at Rovitracker.inc',
+        phoneNumber: '801-691-4362',
+        email: '',
+        HIGTK: '',
+      },
+    ],
+  }
+
   render() {
     return (
       <div className='Refferences'>
         <div className='RefferenceName'>Refferences</div>
+        {this.state.refferences.map((refference) => (
+          <div className='RefferenceContainer'>
+            <div>
+              {refference.name} - {refference.phoneNumber}
+            </div>
+            <div>{refference.email}</div>
+            <div>{refference.affiliation}</div>
+            <div className='RefferenceDivder' />
+          </div>
+        ))}
       </div>
     )
   }
